d8ceb5ace87f79dbf8826e658da6da5ea5893de6
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-19 13:50:12 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg")
+
+with col2:
+    st.title("Phumlani Dlamini | Python Developer")
+    content = """Enthusiastic Python developer with a strong focus on blockchain (Web3) development. I bring versatility to the table, skilled in both web development and machine learning.  2x Web3 Hackathon winner, eager to leverage my skills to build secure and innovative smart contracts.
+
+A year of experience as a web developer, proficient in utilizing technologies like AWS and Jira. Let's connect and discuss your Python development needs!
+"""
+    st.info(content)
+content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me!"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
